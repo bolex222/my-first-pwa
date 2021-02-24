df4c75d662aca128ac06158dffcc83389789b77f
Basile LECOUTURIER <lecouturier.basile@gmail.com>
2021-02-24 21:27:13 +0100

    change sw url

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 if (navigator.serviceWorker) {
-  navigator.serviceWorker.register('/sw.js').then(() => {
+  navigator.serviceWorker.register('/my-first-pwa/sw.js').then(() => {
     console.log('Service Worker Registered');
   })
 }
